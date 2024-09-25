2f5a161145fb83699a569c92f19001d55ad6d6a9
nthanq2505 <20127627@student.hcmus.edu.vn>
2024-09-25 20:17:19 +0700

    Quick Start: Complete!!!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
-import ComponentA from "./components/ComponentA/ComponentA";
+// import ComponentA from "./components/ComponentA/ComponentA";
+import MyApp from "./App";
 
 document.body.innerHTML = '<div id="app"></div>';
 
 const root = createRoot(document.getElementById("app"));
-root.render(<ComponentA />);
+root.render(<MyApp />);
